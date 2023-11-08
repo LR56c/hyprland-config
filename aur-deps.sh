97c1a1a087682982f1cc25e20833b056de25a1a4
LR56c <ronalli56@gmail.com>
2023-11-08 14:23:30 -0300

    update notebook config and aur deps bash

---
+++ aur-deps.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+yay -Syyu --noconfirm
+yay -S aur/anyrun-git -a
+yay -S aur/exercism-bin -a
+yay -S aur/hyprpicker-git -a
+yay -S aur/rofi-file-browser-extended-git -a
+yay -S aur/snapd -a
+yay -S aur/textsnatcher -a
+yay -S aur/pyprland -a
+yay -S aur/appimagelauncher -a
+yay -S aur/emote -a
+yay -Syyu --noconfirm
